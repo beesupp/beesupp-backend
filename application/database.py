5913793e46e84ecd24ed65033d161df25aa99717
sirmordred <oguzhan97yigit@gmail.com>
2021-12-11 21:54:45 +0300

    backend: db: Add basic CRUD functions

--- application/database.py
+++ application/database.py
@@ -27,3 +27,63 @@ def init_database_tables():
     # If table don't exist, Create.
     if not engine.dialect.has_table(engine, "user"):
         Base.metadata.create_all(bind=engine)
+
+def create_user(user_name):
+    contains = bool(User.query.filter_by(name=user_name).first())
+    if not contains:  # if not contains, add it
+        new_user = User(name=user_name)
+        db_session.add(new_user)
+        db_session.commit()
+
+def create_vehicle(user_name, user_vehicle_name):
+    selected_user = User.query.filter_by(name=user_name).first()
+    if selected_user is not None:
+        selected_user_vehicles = User.query.join(MercedesVehicle).filter(
+            User.name == user_name).filter(MercedesVehicle.name == user_vehicle_name).all()
+        # if selected user does not have specified vehicle, insert it
+        if len(selected_user_vehicles) == 0:
+            selected_user_entry = User.query.get(selected_user.id)
+            new_vehicle = MercedesVehicle(user_vehicle_name)
+            selected_user_entry.vehicle.append(new_vehicle)
+            db_session.commit()
+
+def create_vehicle_item(item_name, item_vehicle_name, item_user_name):
+    selected_vehicle = MercedesVehicle.query.join(User).filter(
+        User.name == item_user_name).filter(MercedesVehicle.name == item_vehicle_name).first()
+    if selected_vehicle is not None:
+        selected_vehicle_items = User.query.join(MercedesVehicle).join(MercedesVehicleItem).filter(User.name == item_user_name).filter(
+            MercedesVehicle.name == item_vehicle_name).filter(MercedesVehicleItem.name == item_name).all()
+        if len(selected_vehicle_items) == 0:
+            new_item = MercedesVehicleItem(item_name)
+            selected_vehicle.vehicle_item.append(new_item)
+            db_session.commit()
+
+def get_all_users():
+    all_users_arr = []
+    all_users = db_session.query(User)
+    for user in all_users:
+        each_user = {}
+        each_user['name'] = user.name
+        all_users_arr.append(each_user)
+        print("User: " + user.name)
+    return all_users_arr
+
+def get_all_vehicles():
+    all_vehicles_arr = []
+    all_vehicles = db_session.query(MercedesVehicle)
+    for vehicle in all_vehicles:
+        each_vehicle = {}
+        each_vehicle['name'] = vehicle.name
+        all_vehicles_arr.append(each_vehicle)
+        print("Vehicle listed: " + vehicle.name)
+    return all_vehicles_arr
+
+def get_all_vehicle_items():
+    all_items_arr = []
+    all_mercedes_vehicle_items = db_session.query(MercedesVehicleItem)
+    for item in all_mercedes_vehicle_items:
+        each_item = {}
+        each_item['name'] = item.name
+        all_items_arr.append(each_item)
+        print("Item listed: " + item.name)
+    return all_items_arr
